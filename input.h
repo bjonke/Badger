b86bbd92acb59234f7ee82caa39d4a455fcad553
Me <bjonke@users.noreply.github.com>
2016-11-05 18:07:06 +0100

    Update input.h

--- input.h
+++ input.h
@@ -1,9 +1,6 @@
 #ifndef INPUT_H
 #define INPUT_H
 
-#define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 1 : 0)
-#define KEY_UP(vk_code) ((GetAsyncKeyState(vk_code) & 0x8000) ? 0 : 1)
-
 const int FieldSize = 5;
 
 class TilesSDL
@@ -27,36 +24,6 @@ class PlayerSDL
 	int Acorns;
 	bool BadAcorn;
 };
- 
-/*
-struct TILES
-{
-	HBITMAP hBadAcorn;
-	BITMAP BadAcorn;
-	HBITMAP hAcorn;
-	BITMAP Acorn;
-	HBITMAP hWall;
-	HBITMAP hExit;
-	BITMAP Wall;
-	BITMAP Exit;
-	HBITMAP hIntro;
-	BITMAP Intro;
-};
-
-struct PLAYER
-{
-	int x;
-	int y;
-	int Score;
-	int Velocity;
-	double Acorns;
-	HBITMAP hIcon;
-	BITMAP Icon;
-	
-	bool BadAcorn;
-	
-};
-*/
 
 class InputSDL
 {
@@ -89,45 +56,4 @@ class InputSDL
 	void KeyPress();
 };
 
-/*
-class CInput
-{
-private:
-
-	int Splash;
-	PLAYER Character;
-	PLAYER Character2;
-
-	int Col;
-	int Row;
-	int FieldX;
-	int FieldY;
-	
-public:
-
-	int AcornField[FieldSize][FieldSize];
-	
-	bool Collecting();
-	bool Sick();
-	bool CheckForAcorns();
-	void SpreadAcorns();
-	int Acorns();
-	int Acorns2();
-	int Score();
-	int Score2();
-	int Xloc();
-	int Yloc();
-	int Xloc2();
-	int Yloc2();
-	void KeyPress();
-	int ShowSplash();
-
-	void Reset();
-
-	int GameFinished();
-	CInput();
-	CInput::CInput(int x, int y);
-	~CInput() {};
-};
 #endif
-*/
